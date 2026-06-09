d9db99d0241789ec6569e6e37881b164af0055d1
Kazım Etiksan <kazim@pikselmutfak.com>
2026-06-09 13:52:56 +0300

    Allow blob images in CSP

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,14 @@ if (env.nodeEnv !== 'production') {
   );
 }
 
-app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      'img-src': ["'self'", 'data:', 'https://*.blob.core.windows.net']
+    }
+  },
+  crossOriginResourcePolicy: { policy: 'cross-origin' }
+}));
 app.use(morgan(env.nodeEnv === 'production' ? 'combined' : 'dev'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
